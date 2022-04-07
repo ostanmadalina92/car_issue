9f1239cc00d511b44580edd1f0f2050fb6d66c98
ostanmadalina92 <ostan.madalina@yahoo.com>
2022-04-07 17:57:57 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const questions = [
+  {
+    text: "Is the car silent when you turn the key?",
+    yes: "corroded",
+    no: "clicking",
+  },
+  {
+    text: "Are the battery terminals corroded?",
+    yes: "clean",
+    no: "cables",
+    id: "corroded",
+  },
+  {
+    text: "Does the car make a clicking noise?",
+    yes: "battery",
+    no: "crank",
+    id: "clicking",
+  },
+  {
+    text: "Does the car crank up but fail to start?",
+    yes: "spark",
+    no: "engine",
+    id: "crank",
+  },
+  {
+    text: "Does the engine start and then die?",
+    yes: "injection",
+    no: "answer",
+    id: "engine",
+  },
+  {
+    text: "No more questions here.",
+    id: "answer",
+  },
+  {
+    text: "Does your car have fuel injection?",
+    yes: "choke",
+    no: "service",
+    id: "injection",
+  },
+  { text: "Clean terminals and try starting again.", id: "clean" },
+  { text: "Replace cables and try again.", id: "cables" },
+  { text: "Replace the battery.", id: "battery" },
+  { text: "Check spark plug connections.", id: "spark" },
+  { text: "Get it in for service.", id: "service" },
+  { text: "Check to ensure the choke is opening and closing.", id: "choke" },
+];
 
 function App() {
+  const [answer, setAnswer] = useState(questions[0]);
+  const [toggle, setToggle] = useState(false);
+
+  function handleClick(decision) {
+    if (decision === true) {
+      const getAdviceY = questions.find(
+        (question) => question.id === answer.yes
+      );
+      setAnswer(getAdviceY);
+      const getOtherAdvice = !getAdviceY.hasOwnProperty("yes");
+      if (getOtherAdvice) {
+        setToggle(true);
+      }
+    }
+
+    if (decision === false) {
+      const getAdviceN = questions.find(
+        (question) => question.id === answer.no
+      );
+      setAnswer(getAdviceN);
+      const getOtherAdvice = getAdviceN.hasOwnProperty("no");
+      if (!getOtherAdvice) {
+        setToggle(true);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 id="top-q">{answer.text}</h2>
+      <div className="container">
+        <div className="side">
+          <button id="left" disabled={toggle} onClick={() => handleClick(true)}>
+            Yes
+          </button>
+        </div>
+        <div className="side">
+          <button id="right" disabled={toggle}onClick={() => handleClick(false)}>
+            No
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
